0bf139db17db7db1fb8a8f011cb2c2aaf1631ed9
Cheastear <56431973+Cheastear@users.noreply.github.com>
2024-12-15 18:29:27 +0200

    auth test

---
+++ src/auth/auth.service.spec.ts
@@ -0,0 +1,50 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import PayloadType from 'src/types/PayloadType';
+import { AuthGuard } from './auth.guard';
+import { SharedJwtAuthModule } from 'src/shared-jwt-auth/shared-jwt-auth.module';
+import { AuthModule } from './auth.module';
+import { ConfigModule } from '@nestjs/config';
+
+describe('AuthService', () => {
+  let authService: AuthService;
+  let authGuard: AuthGuard;
+
+  const payloadExample: PayloadType = {
+    id: 1,
+    username: 'JMTheBest',
+    role: 'waiter',
+    email: 'JM@mail.com',
+  };
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        SharedJwtAuthModule,
+        ConfigModule.forRoot({
+          envFilePath: '.env',
+          isGlobal: true,
+        }),
+      ],
+      providers: [AuthService, AuthGuard],
+    }).compile();
+
+    authService = module.get<AuthService>(AuthService);
+    authGuard = module.get<AuthGuard>(AuthGuard);
+  });
+
+  it('should be defined', async () => {
+    expect(authService).toBeDefined();
+  });
+
+  it('should create and validate token', async () => {
+    const jwtToken = await authService.login({ ...payloadExample });
+
+    expect(jwtToken).toBeDefined();
+    expect({
+      ...(await authGuard.validateToken(jwtToken.access_token)),
+      exp: undefined,
+      iat: undefined,
+    }).toEqual(payloadExample);
+  });
+});
