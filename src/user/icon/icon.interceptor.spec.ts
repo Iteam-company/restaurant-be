5d77e4baacc11635ec0bf82f2786c5ad1461b8fd
Cheastear <56431973+Cheastear@users.noreply.github.com>
2024-12-23 16:04:58 +0200

    remove test

--- src/user/icon/icon.interceptor.spec.ts
+++
@@ -1,7 +0,0 @@
-import { IconInterceptor } from './icon.interceptor';
-
-describe('IconInterceptor', () => {
-  it('should be defined', () => {
-    expect(new IconInterceptor()).toBeDefined();
-  });
-});
