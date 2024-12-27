0a9b4b01e3bfdbee1bfa247c18369da231839a6e
Cheastear <56431973+Cheastear@users.noreply.github.com>
2024-12-27 10:38:30 +0200

    add view query params in swagger

--- src/restaurant/dto/search-query.dto.ts
+++ src/restaurant/dto/search-query.dto.ts
@@ -1,8 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { PaginateQuery } from 'nestjs-paginate';
 
 class SearchQueryDto implements PaginateQuery {
+  @ApiProperty({ description: 'Limit on one page' })
   limit?: number;
+  @ApiProperty({ description: 'Page number' })
   page?: number;
+  @ApiProperty({ description: 'Search by name or address of restaurant' })
   search?: string;
   path: string;
 }
