342b99ffd74cc5dfa50577bc498d24e11829519c
Kyryll Vitoshkin <56431973+Cheastear@users.noreply.github.com>
2025-10-16 14:24:53 +0300

    feat: swagger description for generate quesetion

--- src/quiz/dto/generate-questions.dto.ts
+++ src/quiz/dto/generate-questions.dto.ts
@@ -8,14 +8,52 @@ class GenerateQuestionsDto {
   @ApiProperty({
     description: 'Count of quizzes(by default 5)',
     required: false,
+    example: 5,
   })
   count?: number;
 
   @ApiProperty({
-    type: 'array',
+    isArray: true,
     description:
       'Previous question based on this ai should not generate same questions',
     required: false,
+    example: `
+[
+  {
+   "id":1,
+   "text":"Які інгредієнти входять до складу коктейлю «Long Island Ice Tea»?",
+   "variants":[
+      "Ром Captain Morgan White",
+      "Джин Gordon's London Dry",
+      "Текіла Jarana Blanco",
+      "Горілка Rada Premium",
+      "Вермут Cinzano Rosso",
+      "Coca-Cola"
+   ],
+   "correct":[
+      0,
+      1,
+      2,
+      3,
+      5
+   ],
+   "multipleCorrect":true
+  },
+  {
+    "id":2,
+    "text":"Який вихід має фреш апельсиновий?",
+    "variants":[
+        "100",
+        "150",
+        "200",
+        "250"
+    ],
+    "correct":[
+        2
+    ],
+    "multipleCorrect":false
+  }
+]`,
   })
   previousQuestions?: Array<Question>;
 }
